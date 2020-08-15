76b4f3c566e5b5c690f76d2129827d749d637d85
jamesmcgill <jamesmcgill@users.noreply.github.com>
2020-08-15 22:43:39 +0200

    render gradient image instead of solid colour

--- src/main.zig
+++ src/main.zig
@@ -5,26 +5,49 @@ const std = @import("std");
 
 //------------------------------------------------------------------------------
 pub fn main() anyerror!void {
-  var raw: [256*256*4]u8 = undefined;
-  for (raw) |*item, i|
+  const IMAGE_FILENAME = "test.bmp";
+  const IMAGE_WIDTH: u32 = 256;
+  const IMAGE_HEIGHT: u32 = 256;
+  const NUM_COMPONENTS: u32 = 4;
+
+  var pixels: [IMAGE_WIDTH * IMAGE_HEIGHT * NUM_COMPONENTS]u8 = undefined;
+
+  // Fill with background color
+  // Byte-by-byte to avoid endianness concerns
+  const xRange: f32 = @intToFloat(f32, IMAGE_WIDTH - 1);
+  const yRange: f32 = @intToFloat(f32, IMAGE_HEIGHT - 1);
+  const componentsPerRow = NUM_COMPONENTS * IMAGE_WIDTH;
+
+  for (pixels) |*item, it|
   {
-    const channel = @mod(i, 4);
+    const i = @intCast(u32, it);
+    // The current pixel's coordinate position (row, col)
+    const row:u32 = i / componentsPerRow;
+    const col:u32 = (i % componentsPerRow) / NUM_COMPONENTS;
+
+    // The current pixel's normalised position [0, 1]
+    const x:f32 = @intToFloat(f32, col) / xRange;
+    const y:f32 = @intToFloat(f32, row) / yRange;
+
+    // std.debug.warn("Col:{}, Row:{}, X:{}, Y{}\n", .{col, row, x, y});
+    const channel = i % NUM_COMPONENTS;
     item.* = switch (channel)
     {
-      0 => 0x00,  // R
-      1 => 0x00,  // G
-      2 => 0xff,  // B
-      3 => 0xff,  // A
+      0 => @floatToInt(u8, x * 255.0),  // Red
+      1 => @floatToInt(u8, y * 255.0),  // Green
+      2 => 0x30,  // Blue
+      3 => 0xff,  // Alpha
       else => 0x00,
     };
   }
-  
+
+  // Save the image to a file
   var f = c.stbi_write_bmp(
-    "test.bmp",
-    @as(c_int, 256),
-    @as(c_int, 256),
-    @as(c_int, 4),
-    @ptrCast(*const c_void, &raw[0])
+    IMAGE_FILENAME,
+    @as(c_int, IMAGE_WIDTH),
+    @as(c_int, IMAGE_HEIGHT),
+    @as(c_int, NUM_COMPONENTS),
+    @ptrCast(*const c_void, &pixels[0])
   );
 
   std.debug.warn("All your codebase are belong to us.: {}\n", .{f});
