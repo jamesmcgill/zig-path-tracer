2085af166e2e730e15e6ee854b309149d89988af
jamesmcgill <jamesmcgill@users.noreply.github.com>
2020-08-27 09:51:08 +0200

    sphere: create struct to instance sphere objects

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,8 @@ const Vec3 = @import("Vec3.zig").Vec3;
 const Ray = @import("Ray.zig").Ray;
 
 //------------------------------------------------------------------------------
-const Color = packed struct {
+const Color = packed struct
+{
     red: u8,
     green: u8,
     blue: u8,
@@ -18,59 +19,74 @@ const Color = packed struct {
 };
 
 //------------------------------------------------------------------------------
-const CollisionInfo = struct {
+const CollisionInfo = struct
+{
     point: Vec3,
     t: f32,
     surface_normal: Vec3,
 };
 
 //------------------------------------------------------------------------------
-pub fn hitsSphere(ray: Ray) ?CollisionInfo {
-  const sphere_pos = Vec3.init(0.0, 0.0, 110.0);
-  const sphere_radius = 50.0;
+const Sphere = struct
+{
+    position: Vec3,
+    radius: f32,
+    color: Vec3,
 
-  const displacement = ray.origin.subtract(sphere_pos);
+    pub fn hitTest(self: Sphere, ray: Ray, t_min: f32, t_max: f32) ?CollisionInfo
+    {
+      const displacement = ray.origin.subtract(self.position);
 
-  const a: f32 = Vec3.dot(ray.direction, ray.direction);
-  const b: f32 = 2.0 * Vec3.dot(displacement, ray.direction);
-  const c: f32 = Vec3.dot(displacement, displacement) -
-    (sphere_radius * sphere_radius);
+      const a: f32 = Vec3.dot(ray.direction, ray.direction);
+      const b: f32 = 2.0 * Vec3.dot(displacement, ray.direction);
+      const c: f32 = Vec3.dot(displacement, displacement) -
+        (self.radius * self.radius);
 
-  const discriminant: f32 = (b * b) - (4.0 * a * c);
-  if (discriminant < 0.0)
-  {
-    return null;
-  }
+      const discriminant: f32 = (b * b) - (4.0 * a * c);
+      if (discriminant < 0.0) { return null; }
 
-  const discrim_root = math.sqrt(discriminant);
-  const double_a = 2.0 * a;
-  const t1 = (-b + discrim_root) / double_a;
-  const t2 = (-b - discrim_root) / double_a;
+      const discrim_root = math.sqrt(discriminant);
+      const double_a = 2.0 * a;
+      const t1 = (-b + discrim_root) / double_a;
+      const t2 = (-b - discrim_root) / double_a;
 
-  // Determine closest t
-  var t = t1;
-  if (t2 < t1 and t2 >= 0.0)
-  {
-    t = t2;
-  }
-  if (t < 0.0)
-  {
-    return null;
-  }
+      if (closestValidT(t1, t2, t_min, t_max)) |t|
+      {
+        const point_at_t = ray.pointAtT(t);
+        return CollisionInfo
+        {
+          .point = point_at_t,
+          .t = t,
+          .surface_normal = point_at_t.subtract(self.position),
+        };
+      }
+      return null;
+    }
 
-  const point_at_t = ray.pointAtT(t);
-  const info = CollisionInfo {
-    .point = point_at_t,
-    .t = t,
-    .surface_normal = point_at_t.subtract(sphere_pos),
-  };
+    pub fn isInRange(val: f32, min: f32, max: f32) bool
+    {
+      if (val < min) { return false; }
+      if (val > max) { return false; }
+      return true;
+    }
 
-  return info;
-}
+    pub fn closestValidT(t1: f32, t2: f32, t_min: f32, t_max: f32) ?f32
+    {
+      var isT1Valid = isInRange(t1, t_min, t_max);
+      var isT2Valid = isInRange(t2, t_min, t_max);
+
+      if (!isT1Valid and !isT2Valid) { return null; }
+      if (isT1Valid and !isT2Valid) { return t1; }
+      if (isT2Valid and !isT1Valid) { return t2; }
+
+      return if (t1 < t2) t1 else t2;
+    }
+};
 
 //------------------------------------------------------------------------------
-pub fn calcColor(ray: Ray) Vec3 {
-  if (hitsSphere(ray)) |info|
+pub fn calcColor(ray: Ray, sphere: Sphere) Vec3
+{
+  if (sphere.hitTest(ray, 0.0, 100000.0)) |info|
   {
     return info.surface_normal.normalized().rescaled();
   }
@@ -85,12 +101,22 @@ pub fn calcColor(ray: Ray) Vec3 {
 }
 
 //------------------------------------------------------------------------------
-pub fn main() anyerror!void {
+pub fn main() anyerror!void
+{
   // Output image details
   const image_filename = "test.bmp";
   const image_width: u32 = 256;
   const image_height: u32 = 256;
 
+  // Scene objects
+  const sphere = Sphere
+  {
+    .position = Vec3.init(0.0, 0.0, 110.0),
+    .radius = 50.0,
+    .color = Vec3.init(1.0, 0.0, 0.0),
+  };
+
+  // Output image
   var pixels: [image_width * image_height]Color = undefined;
 
   // Frustum extents
@@ -125,7 +151,7 @@ pub fn main() anyerror!void {
     const ray_dir: Vec3 = to_pixel.subtract(ray_origin);
     const ray = Ray.init(ray_origin, ray_dir);
 
-    const color: Vec3 = calcColor(ray);
+    const color: Vec3 = calcColor(ray, sphere);
     item.red = @floatToInt(u8, color.x * 255.0);
     item.green = @floatToInt(u8, color.y * 255.0);
     item.blue = @floatToInt(u8, color.z * 255.0);
